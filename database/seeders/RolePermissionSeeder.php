d58ca184ef91f0731a08e11462f989d0b671c96a
kingslyDev <ghalyallcoc@gmail.com>
2024-05-23 22:33:44 +0700

    new coding

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            'view course',
+            'create course',
+            'edit course',
+            'delete course',
+        ];
+
+        foreach($permissions as $permission){
+            Permission::create([
+                'name' => $permission
+            ]);
+        }
+
+        $teacherRole = Role::create([
+            'name' => 'teacher'
+        ]);
+
+        $teacherRole->givePermissionTo([
+            'view course',
+            'create course',
+            'edit course',
+            'delete course',
+        ]);
+
+        $studentRole = Role::create([
+            'name' => 'student'
+        ]);
+
+        $studentRole->givePermissionTo([
+            'view course',
+        ]);
+
+        //User SuperAdmin
+       $user = User::create([
+        'name' => 'Bejo',
+        'email' => 'bejo@suhendar.com',
+        'password' => bcrypt("123456"),
+       ]);  
+
+       $user->assignRole($teacherRole);  //
+    }
+}
